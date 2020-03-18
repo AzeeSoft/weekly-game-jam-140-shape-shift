f690e0f60fb43d92dedf4b569873d28a76918496
Azee <aziztitu1996@gmail.com>
2020-03-17 20:41:12 -0500

    Adds common tools and libraries

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Image healthBar;
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI highscoreText;
+    public TextMeshProUGUI scoreText;
+
+    public Image damageIndicatorImage;
+    public float damageIndicatorMaxOpacity = 0.7f;
+    public float damageIndicatorAnimationDuration = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //LevelManager.Instance.planetHealth.OnDamageTaken.AddListener(IndicateDamage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHealthUI();
+    }
+
+    void UpdateHealthUI()
+    {
+        /*healthBar.fillAmount = LevelManager.Instance.planetHealth.normalizedHealth;
+        healthText.text = $"{LevelManager.Instance.planetHealth.currentHealth:##0}";
+        highscoreText.text = $"Highscore: {Mathf.Max(LevelManager.Instance.score, LevelManager.Instance.GetHighScore()):###,##0}";
+        scoreText.text = $"Score: {LevelManager.Instance.score:###,##0}";*/
+    }
+
+    void IndicateDamage()
+    {
+        damageIndicatorImage.DOKill();
+
+        var tween = damageIndicatorImage.DOFade(damageIndicatorMaxOpacity, damageIndicatorAnimationDuration);
+        tween.OnComplete(() =>
+        {
+            damageIndicatorImage.DOKill();
+            damageIndicatorImage.DOFade(0, damageIndicatorAnimationDuration).Play();
+        });
+
+        tween.Play();
+    }
+}
---
+++ Assets/Scripts/UI/MenuPage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MenuPage : MonoBehaviour
+{
+    public float fadeDuration = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Show()
+    {
+        var preSeq = gameObject.DOFade(0, 0);
+        var showSeq = gameObject.DOFade(1, fadeDuration);
+
+        preSeq.AppendCallback(() =>
+        {
+            gameObject.SetActive(true);
+            showSeq.Play();
+        });
+
+        preSeq.Play();
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(true);
+
+        var preSeq = gameObject.DOFade(1, 0);
+        var hideSeq = gameObject.DOFade(0, fadeDuration);
+
+        preSeq.AppendCallback(() =>
+        {
+            hideSeq.Play();
+        });
+
+        hideSeq.AppendCallback(() =>
+        {
+            gameObject.SetActive(false);
+        });
+
+        preSeq.Play();
+    }
+}

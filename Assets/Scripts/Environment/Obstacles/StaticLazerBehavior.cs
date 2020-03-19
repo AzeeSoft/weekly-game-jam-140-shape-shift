0dda51f55ca854ac683a7fbb9720646be3a496e9
William Gavin Lacalle <digitalbros7@gmail.com>
2020-03-19 02:55:53 -0500

    Creates Static Lazer and Changes Lazer color

---
+++ Assets/Scripts/Environment/Obstacles/StaticLazerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticLazerBehavior : MonoBehaviour
+{
+    public float damage = 10;
+
+    void OnTriggerEnter(Collider other)
+    {
+        FlightModel otherFM = other.gameObject.GetComponent<FlightModel>();
+
+        if (otherFM != null)
+        {
+            print("Hit Lazer!");
+            otherFM.health.TakeDamage(damage);
+        }
+    }
+}

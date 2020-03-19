a7884c0decb44335667ff5d25737d4651c3d26df
William Gavin Lacalle <digitalbros7@gmail.com>
2020-03-19 07:30:16 -0500

    Adds juice to Main Menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 using TMPro;
@@ -8,7 +9,10 @@ using TMPro;
 public class MainMenu : MonoBehaviour
 {
     public string sceneToLoad;
+    public TextMeshProUGUI mainMenuTextMesh;
     public TextMeshProUGUI highscoreTextMesh;
+    public Text dummyText;
+    public float textChangeSpeed;
 
     Sequence textColorSequence;
     public float colorChangeSpeed;
@@ -24,7 +28,24 @@ public class MainMenu : MonoBehaviour
         textColorSequence = DOTween.Sequence();
         textColorSequence.Append(highscoreTextMesh.DOColor(textColor2, colorChangeSpeed)).Append(highscoreTextMesh.DOColor(textColor1, colorChangeSpeed)).SetLoops(-1);
 
+        const string glyphs = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (int i = 0; i < 13; i++)
+        {
+            dummyText.text += glyphs[Random.Range(0, glyphs.Length)];
+        }
+
+        dummyText.DOText("Astral Angles", textChangeSpeed, true, ScrambleMode.All);
+        
         Time.timeScale = 1;
+    }
+
+    void Update()
+    {
+        if (mainMenuTextMesh.text != "Astral Angles")
+        {
+            mainMenuTextMesh.text = dummyText.text;
+        }
     }
 
     public void loadNextScene()
